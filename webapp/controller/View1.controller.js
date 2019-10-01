8933351f085de24a6119e82fb6136196a72b8bcd
Abhijeet Deshmukh <abhijeet.deshmukh@incture.com>
2019-10-01 10:39:22 +0000

    master detail template

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,6 +6,20 @@ sap.ui.define([
 	return Controller.extend("com.Deloitte.Deloitte_My_approval_inbox.controller.View1", {
 		onInit: function () {
 
+		},
+			handleSwitchOrientation: function(oEvent) {
+			var sOrientation = this.byId("mySplitContainer").getOrientation();
+			if (sOrientation === "Vertical") {
+				sOrientation = "Horizontal";
+			} else {
+				sOrientation = "Vertical";
+			}
+			this.byId("mySplitContainer").setOrientation(sOrientation);
+		},
+
+		handleToggleSecondaryContent: function(oEvent) {
+			var oSplitContainer = this.byId("mySplitContainer");
+			oSplitContainer.setShowSecondaryContent(!oSplitContainer.getShowSecondaryContent());
 		}
 	});
 });
